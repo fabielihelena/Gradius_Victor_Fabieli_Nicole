f55151970f3a655a15f549bb0114d6aa951eacd9
Victor Fernandes <victorfernandes.nave@gmail.com>
2015-03-25 23:49:23 -0300

    Gradius
    Classic gradius of atari.

---
+++ Assets/Script/GradiusBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GradiusBehaviour : Default {
+    public float speed;
+    public float hp;
+    public string stats;
+    public float horizontal;
+    public float vertical;
+	// Use this for initialization
+	void Awake () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = Input.GetAxisRaw("Horizontal") ;
+        vertical = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(speed * horizontal * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Script/Space_Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Space_Move : Default {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.renderer.material.mainTextureOffset += new Vector2(-1 * Mathf.Pow( Time.deltaTime,2),0);
+	}
+}
