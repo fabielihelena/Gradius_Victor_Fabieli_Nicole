d64e48b83a5a6aca15b6561fcb7d9ea088b14eed
zappknight <victorfernandes.nave@gmail.com>
2015-04-01 11:49:38 -0300

    Destruição do Chefão

---
+++ Assets/Script/BossBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBehaviour : Default {
+
+    public float speedz;
+    public int life = 20;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Colidiu: " + col.gameObject.name);
+        if (col.gameObject.tag == "Bullet")
+        {
+            Debug.Log("LOL");
+            life--;
+            Destroy(col.gameObject);
+        }
+    }
+   
+	// Update is called once per frame
+	void Update () {
+        if (life <= 0)
+        {
+            Debug.Log(life);
+            Destroy(transform.parent.gameObject);
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Script/BossMove.cs
+++ Assets/Script/BossMove.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Default : MonoBehaviour {
+public class BossMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
